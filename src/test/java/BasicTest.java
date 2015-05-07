ab3d6401e814412576236cb4f2ecbd7b5108b5eb
honma <honma@ebay.com>
2015-05-07 18:44:47 +0800

    first commit

---
+++ src/test/java/BasicTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Hongbin Ma(Binmahone) on 5/7/15.
+ */
+public class BasicTest {
+    @Test
+    public void foo() {
+        Assert.assertEquals(1, 1);
+    }
+}
